ad1d79491810149ecc8435b73be7593fb98b2524
Raita22 <113097574+Raita22@users.noreply.github.com>
2023-03-03 19:16:13 +0700

    Add files via upload

---
+++ pertemuan-ke-1/P01-22103001006-Raita Rahmatina.php
@@ -0,0 +1,40 @@
+<?php
+//kode dibawah ini merupakan contoh variabel
+$nama = "Raita";
+$umur = 20;
+echo "Halo, nama saya" $nama "dan saya berumur" $umur "tahun";
+?>
+
+<?php
+//kode dibawah ini merupakan contoh operator
+$a = 15;
+$b = 5;
+echo $a + $b; //penjumlahan
+echo $a - $b; //pengurangan
+echo $a * $b; //perkalian
+echo $a / $b; //pembagian
+echo $a % $b; //modulus (sisa pembagian)
+?>
+
+<?php
+//kode dibawah ini merupakan contoh penulisan array
+$numbers = [1, 2, 3, 4, 5];
+?>
+
+<?php
+//kode dibawah ini merupakan contoh penulisan struktur logika (IF ELSE)
+$buah = "manis";
+
+if ($buah >= "semangka") {
+  echo "buah manis";
+} else {
+  echo "buah asam";
+}
+?>
+
+<?php
+//kode dibawah ini merupakan contoh perulangan
+for ($a = 3; $a <= 9 ; $a++) {
+    echo $a;
+  }
+  
