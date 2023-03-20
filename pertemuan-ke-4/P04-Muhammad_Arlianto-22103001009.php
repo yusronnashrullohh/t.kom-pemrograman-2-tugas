11ff382c8eb0c20ffb3a8589f104947cb0e6a574
Li An <113097514+arlianto9999@users.noreply.github.com>
2023-03-20 11:52:57 +0700

    P04-Muhammad_Arlianto-22103001009
    P04-Muhammad_Arlianto-22103001009

---
+++ pertemuan-ke-4/P04-Muhammad_Arlianto-22103001009.php
@@ -0,0 +1,64 @@
+//Tanaman Buah Cucurbitaceae
+<br>
+//Parent adn child
+<br>
+<?php
+
+class Cucurbitaceae{
+    public $nama;
+
+    function __construct($nama)
+    {
+        $this->nama = $nama;
+    }
+
+    function getData(){
+        return "$this->nama";
+    }
+}
+
+
+
+class Semangka extends Cucurbitaceae{
+    public $kulitHalus;
+    function __construct($nama, $kulitHalus)
+    {
+       parent::__construct($nama);
+       $this->kulitHalus =$kulitHalus;
+    }
+    function getData()
+    {
+        return parent::getData() . " - {$this->kulitHalus} Semangka";
+    }
+
+}
+class Melon extends Cucurbitaceae{
+    public $kulitKasar;
+    function __construct($nama, $kulitKasar)
+    {
+       parent::__construct($nama);
+       $this->kulitKasar =$kulitKasar;
+    } 
+    function getData()
+    {
+        return parent::getData() . " - {$this->kulitKasar} Melon";
+    }
+    function isiBatrei(){
+        return "Isi Batrei";
+    }
+}
+
+
+
+$Cucurbitaceae1 = new Semangka("Semangka", "Kulit Halus");
+$Cucurbitaceae2 = new Melon("Melon", "Kulit Kasar");
+
+//this echo will call MobilKonvensional for show of $nama and $merk + $liter for MobilKonvensional
+echo $Cucurbitaceae1->getData();
+// var_dump($trans2);
+//this echo will call MobilListrik for show of $nama and $merk + $liter for MobilListrik
+echo $Cucurbitaceae2->getData();
+
+
+
+?>
